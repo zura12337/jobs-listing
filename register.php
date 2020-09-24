e66a8df51ea11277e8acdd36e275750de3deb3ae
zura12337 <zura.kurta@gmail.com>
2020-09-24 22:06:45 +0400

    Add bootstrap classes in register.php

--- register.php
+++ register.php
@@ -49,51 +49,47 @@ if($_POST){
     <title>JobSite</title>
 </head>
 <body>
-    <header>
-        <h2>Register</h2>
-    </header>
+    <div class="container">
+        <h1>Register</h1>
+        <form action="register.php" method="post">
+            <label for="first_name">First name:</label>
+            <input type="text" class="form-control" name="first_name" id="first_name" value="<?php echo $f_name; ?>">
 
-    <form action="register.php" method="post">
-        <label for="first_name">First name:</label>
-        <input type="text" name="first_name" id="first_name" value="<?php echo $f_name; ?>">
+            <label for="last_name">Last name:</label>
+            <input type="text" class="form-control" name="last_name" id="last_name" value="<?php echo $l_name; ?>">
 
-        <label for="last_name">Last name:</label>
-        <input type="text" name="last_name" id="last_name" value="<?php echo $l_name; ?>">
+            <div class='<?php echo $mail_error_class; ?>'>
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email" id="email" value="<?php echo $email; ?>"><br>
 
-        <div class='<?php echo $mail_error_class; ?>'>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" value="<?php echo $email; ?>"><br>
+                <?php
+                echo $mailError;
+                echo $usernameExists_error;
+                ?>
 
-            <?php
-            echo $mailError;
-            echo $usernameExists_error;
-            ?>
+            </div>
 
-        </div>
+            <label for="mobile">Phone number:</label>
+            <input type="number" class="form-control" name="mobile" id="mobile" value="<?php $phone_number; ?>"><br>
 
-        <label for="mobile">Phone number:</label>
-        <input type="number" name="mobile" id="mobile" value="<?php $phone_number; ?>"><br>
+            <div class="form-check">
+                <label for="individual-check">Individual</label>
+                <input class="form-check-label" type="radio" name="company-individual" id="individual-check">
+            </div>
+            <div class="form-check">
+                <label for="company-check">Company</label>
+                <input class="form-check-label" type="radio" name="company-individual" id="company-check"><br>
+            </div>
 
-        <div>
-            <label for="individual-check">Individual</label>
-            <input type="radio" name="Individual" id="individual-check">
+            <div class='<?php echo $pass_error_class; ?>'>
+                <label for="pass">Password:</label>
+                <input class="form-control" type="password" name="pass" id="pass">
 
-            <label for="company-check">Company</label>
-            <input type="radio" name="company" id="company-check"><br>
-        </div>
-
-        <div class='<?php echo $pass_error_class; ?>'>
-            <label for="pass">Password:</label>
-            <input type="password" name="pass" id="pass">
-
-            <label for="re_pass">Repeat Password:</label>
-            <input type="password" name="re_pass" id="re_pass">
-            <?php echo $passError; ?>
-        </div>
-
-        <label for="submit"></label>
-        <input type="submit" id="submit" value="Submit"><br>
-    </form>
-
+                <label for="re_pass">Repeat Password:</label>
+                <input class="form-control" type="password" name="re_pass" id="re_pass">
+                <?php echo $passError; ?>
+            </div>
+            <input class="btn btn-primary mt-3" type="submit" id="submit" value="Submit"><br>
+        </form>
 </body>
 </html>
