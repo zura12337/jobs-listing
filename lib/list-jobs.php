f804b949eeaf414906423cc5fbfb09641ab14b89
gujjs <g.mekokishvili@omedia.ge>
2020-09-29 20:42:35 +0400

    Add index.php. Implement jobs list function. ინდექსს ცოტა ხელის შევლება უნდა :D თუმცა ფუნქციონირებადია

--- lib/list-jobs.php
+++ lib/list-jobs.php
@@ -1,19 +1,27 @@
 <?php
-// session_start();
+session_start();
 
-// function list_jobs($user_mail){
-//     $json = file_get_contents('database/data.json');
-//     $data = json_decode($json, true);
-//     $answer = array();
-
-//     foreach ($data as $item_id => $content){
-//         if (isset($user_mail)){
-//             echo '1234567890';
-//         }elseif($content["creator-email"] == $){
-
-//         }
-
-//     }
-// }
-
-// list_jobs(null); -->
+function list_jobs($user_mail=null)
+{
+    $json = file_get_contents('../database/data.json');
+    $data = json_decode($json, true);
+    $answer = "<table>
+                   <tr>
+                       <th>Job</th>
+                       <th>Provider</th>
+                       <th>Date</th>
+                   </tr>
+                        ";
+    foreach ($data as $item_id => $content) {
+        if (!isset($user_mail) || $content["creator-email"] == $user_mail){
+            $date = explode(' ', $content["date"])[0];
+            $answer .= "<tr>
+                            <td>{$content["job-name"]}</td>
+                            <td>{$content["creator-name"]}</td>
+                            <td>{$date}</td>
+                        </tr>
+                        ";
+        }
+    }
+    return $answer."</table>";
+}
